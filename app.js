0cb095ce539a7e0934e8f6231580c4bcf3edfe87
wldzimerz <kvkad13@bk.ru>
2021-08-28 18:05:45 +0700

    fix: logic rework

--- app.js
+++ app.js
@@ -8,17 +8,37 @@ const inputType = document.querySelector(".add__type");
 const description = document.querySelector(".add__description");
 const input = document.querySelector(".add__value");
 const income = document.querySelector(".income__list");
+const incomeElements = income.querySelectorAll(".item");
+let i = incomeElements.length;
 const expences = document.querySelector(".expenses__list");
+const expencesElements = expences.querySelectorAll(".item");
+let j = expencesElements.length;
 document.querySelector(".add__btn").addEventListener("click", function () {
-  if (inputType.value == "inc") {
-    totalIncome.innerHTML = Number(totalIncome.innerHTML) + Number(input.value);
-    income.innerHTML = income.innerHTML + "<br>" + input.value;
-  } else {
-    totalExpences.innerHTML = Number(totalExpences.innerHTML) + Number(input.value);
-    expences.innerHTML = expences.innerHTML + "<br>" + input.value;
+  if (inputType.value == "inc" && description.value !== "" && input.value !== "") {
+    totalIncome.innerHTML = (Number(totalIncome.innerHTML) + Number(input.value)).toFixed(2);
+    income.insertAdjacentHTML(
+      "beforeend",
+      `<div class="item clearfix" id="income-${i++}"> <div class="item__description">${
+        description.value
+      }</div> <div class="right clearfix">  <div class="item__value">+ ${Number(input.value).toFixed(
+        2
+      )}</div>          <div class="item__delete">            <button class="item__delete--btn">              <i class="ion-ios-close-outline"></i>            </button>          </div>        </div>      </div>`
+    );
+  } else if (inputType.value == "exp" && description.value !== "" && input.value !== "") {
+    totalExpences.innerHTML = (Number(totalExpences.innerHTML) + Number(input.value)).toFixed(2);
+    expences.insertAdjacentHTML(
+      "beforeend",
+      `<div class="item clearfix" id="expense-${j++}">    <div class="item__description">${
+        description.value
+      }</div>    <div class="right clearfix">      <div class="item__value">- ${Number(input.value).toFixed(
+        2
+      )}</div>      <div class="item__percentage">${Math.round(
+        (Number(input.value) / Number(total.innerHTML)) * 100
+      )}%</div>      <div class="item__delete">        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>      </div>    </div>  </div>`
+    );
   }
   description.value = null;
   input.value = null;
   inputType.value = "inc";
-  total.innerHTML = Number(totalIncome.innerHTML) - Number(totalExpences.innerHTML);
+  total.innerHTML = (Number(totalIncome.innerHTML) - Number(totalExpences.innerHTML)).toFixed(2);
 });
