71de5d4d5ac5d920d9b8e3c1061c511c53b64e7d
wldzimerz <kvkad13@bk.ru>
2021-09-01 11:34:47 +0700

    fix: delete button work fix

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const inputType = document.querySelector(".add__type");
 const description = document.querySelector(".add__description");
 const input = document.querySelector(".add__value");
 const income = document.querySelector(".income__list");
-const incomeElements = income.querySelectorAll(".item");
-let i = incomeElements.length;
+const incomeElements = income.getElementsByClassName("item");
+let incomeListCount = incomeElements.length;
 const expences = document.querySelector(".expenses__list");
-const expencesElements = expences.querySelectorAll(".item");
-let j = expencesElements.length;
+const expencesElements = expences.getElementsByClassName("item");
+let expenceListCount = expencesElements.length;
 const expencesPercentage = document.querySelector(".budget__expenses--percentage");
-document.querySelector(".add__btn").addEventListener("click", function () {
+expencesPercentage.innerHTML = "--";
+
+document.querySelector(".add__btn").addEventListener("click", () => {
   if (inputType.value == "inc" && description.value !== "" && input.value !== "") {
     totalIncome.innerHTML = (Number(totalIncome.innerHTML) + Number(input.value)).toFixed(2);
     income.insertAdjacentHTML(
       "beforeend",
-      `<div class="item clearfix" id="income-${i++}"> <div class="item__description">${
+      `<div class="item clearfix" id="income-${incomeListCount++}"> <div class="item__description">${
         description.value
       }</div> <div class="right clearfix">  <div class="item__value">+ ${Number(input.value).toFixed(
         2
@@ -29,7 +31,7 @@ document.querySelector(".add__btn").addEventListener("click", function () {
     totalExpences.innerHTML = (Number(totalExpences.innerHTML) + Number(input.value)).toFixed(2);
     expences.insertAdjacentHTML(
       "beforeend",
-      `<div class="item clearfix" id="expense-${j++}">    <div class="item__description">${
+      `<div class="item clearfix" id="expense-${expenceListCount++}">    <div class="item__description">${
         description.value
       }</div>    <div class="right clearfix">      <div class="item__value">- ${Number(input.value).toFixed(
         2
@@ -43,11 +45,13 @@ document.querySelector(".add__btn").addEventListener("click", function () {
   inputType.value = "inc";
   total.innerHTML = (Number(totalIncome.innerHTML) - Number(totalExpences.innerHTML)).toFixed(2);
   expencesPercentage.innerHTML = Math.round((Number(totalExpences.innerHTML) / Number(totalIncome.innerHTML)) * 100) + "%";
+  const items = document.querySelectorAll(".item");
+  for (let item of items) {
+    const deleteButtons = item.getElementsByTagName("i");
+    for (let deleteButton of deleteButtons) {
+      deleteButton.addEventListener("click", () => {
+        item.remove();
+      });
+    }
+  }
 });
-
-const deleteButtons = document.querySelectorAll(".item__delete--btn");
-for (let deleteButton of deleteButtons) {
-  deleteButton.addEventListener("click", function () {
-    deleteButton.parentElement.parentElement.parentElement.remove();
-  });
-}
